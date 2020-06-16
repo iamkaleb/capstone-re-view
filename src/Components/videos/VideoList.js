8618a847a637f2fc0964b521f1ccb94c4c73e499
iamkaleb <kalebdmoran@gmail.com>
2020-06-16 14:53:02 -0500

    Categories now listed in main page w/ edit & delete affordances

--- src/Components/videos/VideoList.js
+++ src/Components/videos/VideoList.js
@@ -3,7 +3,14 @@ import React from 'react'
 const VideoList = props => {
 
     return (
-        <h1>Video List</h1>
+        <>
+            <h1>Video List</h1>
+            {props.categories.map(category =>
+                <>
+                    <p key={category.id}>{category.categoryTitle}</p><button>Edit</button><button>Delete</button>
+                </>
+            )}
+        </>
     )
 }
 
