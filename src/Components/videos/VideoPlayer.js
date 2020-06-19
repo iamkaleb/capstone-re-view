06fbffa559841743c546e5554d4cc639dadf5397
iamkaleb <kalebdmoran@gmail.com>
2020-06-19 16:53:46 -0500

    Saving annotations complete

--- src/Components/videos/VideoPlayer.js
+++ src/Components/videos/VideoPlayer.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import ReactPlayer from 'react-player'
 import dataManager from '../../modules/dataManager'
 import '../css/Player.css'
 
 const VideoPlayer = props => {
     
-    const [video, setVideo] = useState({ videoTitle: "", url: "" });
-    const [playing, setPlaying] = useState(false)
+    const [playing, setPlaying] = useState(false);
+    const [video, setVideo] = useState({ videoTitle: '', url: '' });
+    const [notes, setNotes] = useState([]);
+    const [note, setNote] = useState({ noteTitle: '', noteContent: '', timestamp: '', videoId: props.videoId, userId: parseInt(sessionStorage.getItem('user')) });
+    const [isLoading, setIsLoading] = useState(false);
+    const [timestamp, setTimestamp] = useState('');
 
     useEffect(() => {
         dataManager.getByProperty('videos', 'id', props.videoId)
@@ -18,6 +22,90 @@ const VideoPlayer = props => {
         })
     }, [props.videoId]);
 
+    const player = useRef();
+
+    const handleFieldChange = event => {
+        const stateToChange = {...note}
+        stateToChange[event.target.id] = event.target.value;
+        setNote(stateToChange);
+    };
+
+    const getTimestamp = () => {
+        let currentTime = player.current.getCurrentTime()
+        let minutes = Math.floor(currentTime / 60)
+        let seconds = ('0' + Math.floor(currentTime - minutes * 60)).slice(-2);
+
+        const newNote = {...note}
+        newNote.timestamp = `${minutes}:${seconds}`
+
+        setNote(newNote)
+    }
+
+    const applyTimestamp = timestamp => {
+        let minutes = parseInt(timestamp.split(":")[0])
+        let seconds = parseInt(timestamp.split(":")[1])
+
+        let targetTime = (minutes * 60) + seconds
+
+        return player.current.seekTo(targetTime)
+    }
+
+    // Sorting function modified from RobNO's time sorting function found here: https://www.codeproject.com/articles/625832/how-to-sort-date-and-or-time-in-javascript
+    function sortTimestamps(a, b) {
+        let minutesA = parseInt(a.timestamp.split(":")[0])
+        let minutesB = parseInt(b.timestamp.split(":")[0])
+
+        let secondsA = parseInt(a.timestamp.split(":")[1])
+        let secondsB = parseInt(b.timestamp.split(":")[1])       
+
+        let results = minutesA > minutesB ? 1 : minutesA < minutesB ? -1 : 0
+
+        if (results === 0) {
+            results = secondsA > secondsB ? 1 : secondsA < secondsB ? -1 : 0
+        }
+
+        return results
+    }
+
+    const constructNote = event => {
+        event.preventDefault();
+
+        if (note.noteTitle === '') {
+            window.alert('Please title your note!')
+        } else if (note.timestamp === '') {
+            window.alert('Press the "Time" button to timestamp your note!')
+        } else {
+            dataManager.getByProperty('notes', 'timestamp', note.timestamp)
+            .then(noteArr => {
+                if (noteArr > 0) {
+                    window.alert('You already have a timestamp there!')
+                } else {
+                    setIsLoading(true);
+                    const newNote = {...note}
+                    newNote.timestamp =
+                    dataManager.post('notes', newNote)
+                        .then(getNotes)
+                        .then(() => {
+                            setNote({noteTitle: '', noteContent: '', timestamp: '', videoId: props.videoId, userId: parseInt(sessionStorage.getItem('user'))});
+                            setIsLoading(false)
+                        })
+                }
+            })
+        }
+    };
+
+    const getNotes = () => {
+        return dataManager.getAll('notes')
+                .then(notes => {
+                    setNotes(notes)
+                })
+    };
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+
     return (
         <article className='video-player'>
             <ReactPlayer
@@ -25,23 +113,44 @@ const VideoPlayer = props => {
                     url={video.url}
                     playing={playing}
                     controls
+                    ref={player}
                 />
-            <form className='annotation-form'>
-                <input className='annotation-title' type='text' placeholder='Note title'/>
+            <form id='noteForm'>
+                <input 
+                    id='noteTitle' 
+                    type='text' 
+                    placeholder='Note title' 
+                    onChange={handleFieldChange}
+                    value={note.noteTitle}
+                />
                 <textarea
-                className='annotation-content' 
                 placeholder="Add note"
                 id="noteContent"
+                onChange={handleFieldChange}
+                value={note.noteContent}
                 name="noteContent"
                 rows="4"
                 cols="50"
                 />
-                <br />
                 <button
-                    className='annotation-submit'
-                    type="button"
+                    id='noteSubmit'
+                    type='submit'
+                    onClick={constructNote}
                 >Submit</button>
+                <button
+                    id='getTimestamp'
+                    type='button'
+                    onClick={getTimestamp}
+                >Time</button>
+                <p id='timestamp'>{note.timestamp}</p>
             </form>
+            <section id="noteList">
+                {notes.sort(sortTimestamps).map(note => {
+                    return <div className="note" key={note.id}>
+                                <button type="button" onClick={() => applyTimestamp(note.timestamp)}>{note.timestamp}</button><p>{note.noteTitle}</p>
+                            </div>
+                        })}
+            </section>
         </article>
     )
 }
