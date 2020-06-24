4a92b1af1516a263c3731bd24d8d8d1e206ea2e8
iamkaleb <kalebdmoran@gmail.com>
2020-06-24 14:58:23 -0500

    Thumbnails added

--- src/Components/videos/VideoCard.js
+++ src/Components/videos/VideoCard.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const VideoCard = props => {
     
+    const imagePath = () => {
+        const pathId = props.video.url.split('v=')[1]
+        return `https://img.youtube.com/vi/${pathId}/0.jpg`
+    }
+
     return (
         <div className='video-card'>
             <div className='video-card-content'>
+                    <img src={imagePath()} width={250} alt='thumbnail' />
                     <h4 onClick={() => props.history.push(`/videos/${props.categoryTitle}/${props.video.id}`)}>{props.video.videoTitle}</h4>
                     <button onClick={() => props.toggleEditVideoForm(props.video.id)} type='button'>Edit</button>
                     <button onClick={() => props.toggleDeleteVideoConfirm(props.video.id)} type='button'>Delete</button>
